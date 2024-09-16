8dc418f0dcb12b2ed1ad7c728c3a7d54d0bbccec
Paolo Gonzales <paologonzales1616@gmail.com>
2024-09-16 08:54:19 +0800

    STOCK: Design error boundary component

--- src/features/errorBoundary/ErrorBoundary.tsx
+++ src/features/errorBoundary/ErrorBoundary.tsx
@@ -28,14 +28,18 @@ class ErrorBoundary extends React.Component<
     }
 
     return (
-      <div>
-        <h2>Sorry, something went wrong</h2>
-        <p>
-          Unfortunately, we're having a few technical issues and are unable to
-          continue with your application. Please try again later.
-        </p>
-        <br />
-        <button type="button" onClick={this.refreshApp}>
+      <div
+        className="mb-4 text-sm text-red-700 bg-red-100 rounded-lg max-w-lg mx-auto shadow-md p-6 mt-2"
+        role="alert"
+      >
+        <span className="font-medium">Error:</span> Unfortunately, we're having
+        a few technical issues and are unable to continue with your application.
+        Please try again later.
+        <button
+          type="button"
+          onClick={this.refreshApp}
+          className="mt-4 px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500"
+        >
           Try again
         </button>
       </div>
